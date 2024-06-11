9f583557a604459ebfed0db58b8c4586b635a2d8
Eduardo F Zimmermann <z.edufz@gmail.com>
2024-06-11 13:56:59 -0300

    Primeiro commit

---
+++ src/test/java/br/com/project/iniflex/IniflexApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.iniflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IniflexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
